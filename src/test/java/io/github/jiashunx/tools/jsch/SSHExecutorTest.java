672569ac980d570a08b40647444cc0fb70bcc749
jiashunx <wenjiashun@163.com>
2021-01-23 11:11:08 +0800

    upgrade jsch[com.jcraft] version to 0.1.55

--- src/test/java/io/github/jiashunx/tools/jsch/SSHExecutorTest.java
+++ src/test/java/io/github/jiashunx/tools/jsch/SSHExecutorTest.java
@@ -6,8 +6,11 @@ package io.github.jiashunx.tools.jsch;
 public class SSHExecutorTest {
 
     public static void main(String[] args) {
-        SSHRequest request = new SSHRequest("127.0.0.1", "root", "root", "asdf");
+        SSHRequest request = new SSHRequest("192.168.1.7", "jiashunx", "1234.abcd", "asdf");
         SSHResponse response = SSHExecutor.execCommand(request);
+        System.out.println(response);
+        request = new SSHRequest("192.168.1.7", "jiashunx", "1234.abcd", "cat /proc/cpuinfo");
+        response = SSHExecutor.execCommand(request);
         System.out.println(response);
     }
 
